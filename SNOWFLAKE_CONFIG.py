78c42eba3931d57c749985c653ca97aa58a601c6
Talaat Sallam <82365152+talaat259@users.noreply.github.com>
2025-08-10 23:54:32 +0300

    Add files via upload

---
+++ SNOWFLAKE_CONFIG.py
@@ -0,0 +1,9 @@
+SNOWFLAKE_CONFIG = {
+    "account": "VEBAATB-WX72729",
+    "user": "TALAAT",
+    "password": "*g28Lx7h8]JrWqY",
+    "role": "ACCOUNTADMIN",  # optional
+    "warehouse": "COMPUTE_WH",  # Replace with your actual warehouse name
+    "database": "WEATHER",
+    "schema": "PUBLIC"
+}
